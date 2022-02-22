629b1cd71f5029bc1129c5a78ed3aa5e07b670a5
Fragore <alexisfonzos_@hotmail.com>
2022-02-22 15:53:54 -0300

    Fix error

--- carmind_app/lib/api/api_client.dart
+++ carmind_app/lib/api/api_client.dart
@@ -1,4 +1,5 @@
 import 'package:carmind_app/api/pojo/evaluacion/evaluacion.dart';
+import 'package:carmind_app/api/pojo/evaluacion/evaluacion_terminada.dart';
 import 'package:carmind_app/api/pojo/login_pojo.dart';
 import 'package:dio/dio.dart';
 import 'package:retrofit/http.dart';
@@ -6,12 +7,15 @@ import 'package:retrofit/http.dart';
 part 'api_client.g.dart';
 
 @RestApi(baseUrl: "http://vps-1791261-x.dattaweb.com:2233/")
-abstract class ApiClient{
-  factory ApiClient (Dio dio, {String baseUrl}) = _ApiClient;
+abstract class ApiClient {
+  factory ApiClient(Dio dio, {String baseUrl}) = _ApiClient;
 
   @POST("/login")
   Future<TokenLogin> login(@Query("username") String email, @Query("password") String password);
 
   @GET("/evaluacion/{id}")
   Future<Evaluacion> getEvaluacionById(@Path("id") int idEvaluacion);
+
+  @POST("/evaluacion/{id}/realizar")
+  Future<void> realizarEvaluacion(@Path("id") int idEvaluacion, @Body() EvaluacionTerminadaPojo pojo);
 }
--- carmind_app/lib/api/api_client.g.dart
+++ carmind_app/lib/api/api_client.g.dart
@@ -52,6 +52,21 @@ class _ApiClient implements ApiClient {
     return value;
   }
 
+  @override
+  Future<void> realizarEvaluacion(idEvaluacion, pojo) async {
+    const _extra = <String, dynamic>{};
+    final queryParameters = <String, dynamic>{};
+    final _headers = <String, dynamic>{};
+    final _data = <String, dynamic>{};
+    _data.addAll(pojo.toJson());
+    await _dio.fetch<void>(_setStreamType<void>(
+        Options(method: 'POST', headers: _headers, extra: _extra)
+            .compose(_dio.options, '/evaluacion/${idEvaluacion}/realizar',
+                queryParameters: queryParameters, data: _data)
+            .copyWith(baseUrl: baseUrl ?? _dio.options.baseUrl)));
+    return null;
+  }
+
   RequestOptions _setStreamType<T>(RequestOptions requestOptions) {
     if (T != dynamic &&
         !(requestOptions.responseType == ResponseType.bytes ||
