e9219db0afb7614aa92373f363e95fe21bff91ae
Pelli <martinpelli@outlook.com>
2022-06-10 07:37:29 -0300

    CAR-132 | Validar inputs en tiempo real

--- carmind_app/lib/login/view/login_screen.dart
+++ carmind_app/lib/login/view/login_screen.dart
@@ -5,15 +5,13 @@ import 'package:flutter_easyloading/flutter_easyloading.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
 import '../../constants.dart';
+import '../../profile/profile.dart';
+import '../../services/services.dart';
 import '../../widgets/widgets.dart';
-import '../../nueva_contrasena/view/nueva_contrasena.dart';
-import '../../nueva_contrasena/view/ingresar_email.dart';
-import 'package:carmind_app/home/view/navigation_bar.dart';
-import 'package:carmind_app/login/bloc/login_bloc.dart';
-import 'package:carmind_app/nueva_contrasena/view/ingresar_contrasena.dart';
-import 'package:carmind_app/on_boarding/view/on_boarding_content.dart';
-import 'package:carmind_app/profile/bloc/offline_bloc.dart';
-import 'package:carmind_app/profile/bloc/profile_bloc.dart';
+import '../login.dart';
+import 'package:carmind_app/on_boarding/on_boarding.dart';
+import 'package:carmind_app/home/home.dart';
+import 'package:carmind_app/nueva_contrasena/nueva_contrasena.dart';
 
 
 
@@ -87,49 +85,54 @@ class _LoginView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    FormService formService = FormService();
     return Scaffold(
       resizeToAvoidBottomInset: false,
       body: Padding(
         padding: EdgeInsets.only(top: 24, left: 34, right: 34, bottom: MediaQuery.of(context).viewInsets.bottom),
         child: SingleChildScrollView(
           reverse: true,
-          child: Column(
-            children: [
-              const SizedBox(height: 40),
-              SizedBox(
-                child: Image.asset("assets/logo.png", width: 87, height: 87),
+          child: Form(
+            key: formService.keyForm,
+            autovalidateMode: AutovalidateMode.onUserInteraction,
+            child: Column(
+              children: [
+                const SizedBox(height: 40),
+                SizedBox(
+                  child: Image.asset("assets/logo.png", width: 87, height: 87),
+                ),
+                const SizedBox(height: 11),
+                const Text(
+                  "CarMind",
+                  style: TextStyle(fontSize: 25, fontWeight: FontWeight.w700),
+                ),
+                const SizedBox(height: 32),
+                const Text(
+                  "Ingresá a tu cuenta",
+                  style: subtitleStyle,
+                ),
+                const SizedBox(height: 20),
+                CustomInput(controller: emailCon, label: 'E-mail'),
+                const SizedBox(
+                  height: 16,
+                ),
+                CustomInput(controller: passwordCon, label: 'Contraseña', isPassword: true),
+                const SizedBox(height: 52 - 18),
+                CustomElevatedButton(text: 'Iniciar sesión',shapeColor: carMindTopBar, textColor: Colors.white
+                , onPressed:  () =>  ( formService.isValidForm() ) ? BlocProvider.of<LoginBloc>(context).add(AttemptToLogin(emailCon.text, passwordCon.text)) : null),
+                const SizedBox(height: 27 - 8),
+                TextButton(
+                    onPressed: (() => Navigator.push(context, MaterialPageRoute(builder: (context) => const NuevaConstrasena(appBarTitle: 'Restaurar Contraseña', child: IngresarEmail())))),
+                    style: ButtonStyle(
+                      overlayColor: MaterialStateProperty.all(carMindAccentColor.withOpacity(0.2)), 
+                      shape: MaterialStateProperty.all(RoundedRectangleBorder(borderRadius: BorderRadius.circular(10)))),
+                    child: const Text(
+                      "Olvidé mi contraseña",
+                      style: TextStyle(fontSize: 14, fontWeight: FontWeight.w400, color: Colors.black),
+                    ))
+                ],
               ),
-              const SizedBox(height: 11),
-              const Text(
-                "CarMind",
-                style: TextStyle(fontSize: 25, fontWeight: FontWeight.w700),
-              ),
-              const SizedBox(height: 32),
-              const Text(
-                "Ingresá a tu cuenta",
-                style: subtitleStyle,
-              ),
-              const SizedBox(height: 20),
-              CustomInput(controller: emailCon, label: 'E-mail'),
-              const SizedBox(
-                height: 16,
-              ),
-              CustomInput(controller: passwordCon, label: 'Contraseña', isPassword: true),
-              const SizedBox(height: 52 - 18),
-              CustomElevatedButton(text: 'Iniciar sesión',shapeColor: carMindTopBar, textColor: Colors.white
-              , onPressed:  () => BlocProvider.of<LoginBloc>(context).add(AttemptToLogin(emailCon.text, passwordCon.text))),
-              const SizedBox(height: 27 - 8),
-              TextButton(
-                  onPressed: (() => Navigator.push(context, MaterialPageRoute(builder: (context) => const NuevaConstrasena(appBarTitle: 'Restaurar Contraseña', child: IngresarEmail())))),
-                  style: ButtonStyle(
-                    overlayColor: MaterialStateProperty.all(carMindAccentColor.withOpacity(0.2)), 
-                    shape: MaterialStateProperty.all(RoundedRectangleBorder(borderRadius: BorderRadius.circular(10)))),
-                  child: const Text(
-                    "Olvidé mi contraseña",
-                    style: TextStyle(fontSize: 14, fontWeight: FontWeight.w400, color: Colors.black),
-                  ))
-              ],
-            ),
+          ),
         ),
       ),
     );
